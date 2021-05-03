cbff3f64d08fa0c23ddcbd81ddc1c29b45404fae
jkyazhini <42486225+jkyazhini@users.noreply.github.com>
2021-05-03 14:36:26 +0530

    Update TestBookDetails.java

--- src/test/java/in/yazhini/TestBookDetails.java
+++ src/test/java/in/yazhini/TestBookDetails.java
@@ -14,41 +14,41 @@ public class TestBookDetails {
 		System.out.println("\n BOOK DETAILS");
 		System.out.println("BookName-AuthorName-BookPrice-NoOfBooks\n");
 
-		BookDetails user1 = new BookDetails(); /**
+		BookDetails book1 = new BookDetails(); /**
 												 * Store the available books user1.BookName = "Tamil Novels"; And
 												 * authors name,price amount, user1.AuthorName="Thiruvalluvar"; then
 												 * available number of books
 		                                          **/
-		user1.bookName="TamilNovels";
-		user1.authorName="Thiruvalluvar";										
-		user1.bookPrice = "Rs.200";
-		user1.noOfBooks = "10\n";
+		book1.bookName="TamilNovels";
+		book1.authorName="Thiruvalluvar";										
+		book1.bookPrice = "Rs.200";
+		book1.noOfBooks = "10\n";
 
-		BookDetails user2 = new BookDetails();
-		user2.bookName = "English Novels";
-		user2.authorName = "George Orwell";
-		user2.bookPrice = "Rs.250";
-		user2.noOfBooks = "20\n";
+		BookDetails book2 = new BookDetails();
+		book2.bookName = "English Novels";
+		book2.authorName = "George Orwell";
+		book2.bookPrice = "Rs.250";
+		book2.noOfBooks = "20\n";
 
-		BookDetails user3 = new BookDetails();
-		user3.bookName = "Mathematical Discoveries";
-		user3.authorName = "Srinivasa Ramanujan";
-		user3.bookPrice = "Rs.300";
-		user3.noOfBooks = "10\n"; 
+		BookDetails book3 = new BookDetails();
+		book3.bookName = "Mathematical Discoveries";
+		book3.authorName = "Srinivasa Ramanujan";
+		book3.bookPrice = "Rs.300";
+		book3.noOfBooks = "10\n"; 
 
-		BookDetails user4 = new BookDetails();
-		user4.bookName = "GK-India at Risk";
-		user4.authorName = "Jaswant Singh";
-		user4.bookPrice = "Rs.220";
-		user4.noOfBooks = "30\n";
+		BookDetails book4 = new BookDetails();
+		book4.bookName = "GK-India at Risk";
+		book4.authorName = "Jaswant Singh";
+		book4.bookPrice = "Rs.220";
+		book4.noOfBooks = "30\n";
 
 	
 
-		AdminUse.addBookDetails(user1, user2, user3, user4); // Call the AdminUse to display the Books
+		AdminUse.addBookDetails(book1, book2, book3, book4); // Call the AdminUse to display the Books
 		AdminUse.displayBooks();
 
 		int count1 = AdminUse.noOfBooks();
-		System.out.println("Total Count is =" +count1);
+		System.out.println("Total BookName is =" +count1);
 
 		// 3 functionalities - add book , list books, count books
 
