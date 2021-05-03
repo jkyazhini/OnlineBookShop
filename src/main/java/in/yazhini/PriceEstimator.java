c5f875ebcbd9f998d43b87c253fda5302a5f0c4f
jkyazhini <42486225+jkyazhini@users.noreply.github.com>
2021-05-03 14:31:03 +0530

    Update PriceEstimator.java

--- src/main/java/in/yazhini/PriceEstimator.java
+++ src/main/java/in/yazhini/PriceEstimator.java
@@ -37,7 +37,7 @@ public class PriceEstimator {
 	public static double tamilNovels(int A) {
 		double amount = (double) (A * 200);
 		double GST = (double) 7 * (amount / 100);
-		double totalAmount = GST + amount + 50;
+		double totalAmount = GST + amount + 50;  //(Rs.50) is DeliverCharge//
 		System.out.println("Total amount of TamilNovels = " + totalAmount);
 		return totalAmount;
 
@@ -46,7 +46,7 @@ public class PriceEstimator {
 	public static double englishNovels(int B) {
 		double amount = (double) (B * 250);
 		double GST = (double) 7 * (amount / 100);
-		double totalAmount = GST + amount + 50;
+		double totalAmount = GST + amount + 50;  //(Rs.50) is DeliverCharge//
 		System.out.println("Total amount of EnglishNovels = " + totalAmount);
 		return totalAmount;
 	}
@@ -54,7 +54,7 @@ public class PriceEstimator {
 	public static double mathematicsDiscoveries(int C) {
 		double amount = (double) (C * 300);
 		double GST = (double) 7 * (amount / 100);
-		double totalAmount = GST + amount + 50;
+		double totalAmount = GST + amount + 50;    //(Rs.50) is DeliverCharge//
 		System.out.println("Total amount of MathematicsDiscoveries = " + totalAmount);
 		return totalAmount;
 	}
@@ -62,7 +62,7 @@ public class PriceEstimator {
 	public static double gkIndiaAtRisk(int D) {
 		double amount = (double) (D * 350);
 		double GST = (double) 7 * (amount / 100);
-		double totalAmount = GST + amount + 50;
+		double totalAmount = GST + amount + 50;   //(Rs.50) is DeliverCharge//
 		System.out.println("Total amount of GKIndiaAtrisk = " + totalAmount);
 		return totalAmount;
 	}
